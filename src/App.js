17ab78cebfc2f9e5e11725b8b0277720667d22e5
Tudchapong <6210110139@psu.ac.th>
2021-07-08 11:05:23 +0700

    4.Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
 
-function App() {
-  return (
-    <div>
-      <CharacterCard value="h"/>
-      <CharacterCard value="i"/>
-    </div>
-  );
-}
+const word = "Hello";
+  function App() {
+    return (
+      <div>
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
+      </div>
+    );
+  }
 
 export default App;
