39894a05d4df30b051c0dbe98c9094227213a65b
Tudchapong <6210110139@psu.ac.th>
2021-07-08 11:25:18 +0700

    Wordcard

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 export default function CharacterCard(props) {
  return (
  <div className="card">{props.value}</div>
---
+++ src/WordCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function WordCard(props){
+ return (
+    <div>
+        { 
+        Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>) 
+        }
+    </div>
+    );
+}
