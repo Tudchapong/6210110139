24564a1fd2085e9ee6856ae46a3dd192ff43e174
Tudchapong <6210110139@psu.ac.th>
2021-07-08 12:03:52 +0700

    5.Components & States

--- src/WordCard.js
+++ src/WordCard.js
@@ -3,13 +3,12 @@ import CharacterCard from './CharacterCard';
 
 export default function WordCard(props){
     const activationHandler = c =>{
-        console.log(${c}' has been activated.')
+        console.log(`${c} has been activated.`)
     } 
     return (
     <div>
         { 
-        Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler = {activationHandler}/>
-        ) 
+        Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler = {activationHandler}/>) 
         }
     </div>
     );
